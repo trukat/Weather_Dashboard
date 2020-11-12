db57596673e6601bb8684074ebe6c60966f952c8
TruKat <kati.k220@gmail.com>
2020-11-11 23:23:09 -0700

    UPDATE: UV index functional

--- Assets/index.js
+++ Assets/index.js
@@ -1,11 +1,12 @@
 var citySearch;
-var apiKey = '&appid=9bfd908033c6ecbca1dcee4556ecec5b';
-var weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?q=';
-var uviAPI = 'https://api.openweathermap.org/data/2.5/uvi?lat=';
+const apiKey = '&appid=9bfd908033c6ecbca1dcee4556ecec5b';
+const currentDay = moment().format('L');
+let weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?q=';
+var uviAPI = 'https://api.openweathermap.org/data/2.5/uvi?&lat=';
 var forecastAPI = 'https://api.openweathermap.org/data/2.5/forecast?q=';
-var weatherIcon = 'https://openweathermap.org/img/wn/'
+var weatherIconURL = 'https://openweathermap.org/img/wn/'
 var geoAPI = navigator.geolocation;
-var units = 'units=imperial'
+var units = '&units=imperial'
 var searchHistory = [];
 
 $(document).ready(function () {
@@ -19,13 +20,32 @@ $(document).ready(function () {
 function init(city) {
     $.ajax({
         type: 'get',
-        url: weatherAPI + citySearch + apiKey,
+        url: weatherAPI + citySearch + units + apiKey,
         dataType: 'JSON',
         success: function (result) {
-            console.log(result)
+            var weatherIcon = result.weather[0].icon;
+            $('#city-name').text(citySearch + ' (' + currentDay + ') ');
+            $('#forecast-image').attr('src', weatherIconURL + weatherIcon + '.png');
             $("#temperature").html('<b>Temperature: </b>' + result.main.temp + ' °F');
             $("#humidity").html('<b>Humidity: </b>' + result.main.humidity + '%');
             $("#windspeed").html('<b>Windspeed: </b>' + result.wind.speed + ' MPH');
+
+    var lat = result.coord.lat
+    var lon = result.coord.lon
+
+    $.ajax({
+        type: 'get',
+        url: uviAPI + lat + '&lon=' + lon + apiKey,
+        dataType: 'JSON',
+        success: function (uviResult) {
+            var uviResults = uviResult.value
+            $("#uv-index").html('<b>UV Index: </b>' + uviResults);
+        }
+        
+    });
+
+    // if (uvi)
+    
         }
         
     });
