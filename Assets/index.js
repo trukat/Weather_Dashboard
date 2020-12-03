af32ce62e5fae892c69d992c4535d8f48b5cfd09
TruKat <kati.k220@gmail.com>
2020-12-03 13:00:20 -0700

    UPDATE: city list  append functional

--- Assets/index.js
+++ Assets/index.js
@@ -2,16 +2,27 @@ var citySearch;
 const apiKey = '&appid=9bfd908033c6ecbca1dcee4556ecec5b';
 const currentDay = moment().format('L');
 let weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?q=';
-var uviAPI = 'https://api.openweathermap.org/data/2.5/uvi?&lat=';
-var forecastAPI = 'https://api.openweathermap.org/data/2.5/forecast?q=';
-var weatherIconURL = 'https://openweathermap.org/img/wn/'
-var geoAPI = navigator.geolocation;
-var units = '&units=imperial'
+let uviAPI = 'https://api.openweathermap.org/data/2.5/uvi?&lat=';
+let forecastAPI = 'https://api.openweathermap.org/data/2.5/forecast?q=';
+let weatherIconURL = 'https://openweathermap.org/img/wn/';
+let units = '&units=imperial';
+let searchHistoryArr = []
+
 
 $(document).ready(function () {
-    $('#searchButton').on('click', function () {
-        citySearch = $('#city-input').val()
+    $('#searchButton').on('click', function(e) {
+        e.preventDefault();
+        citySearch = $('#city-input').val().trim();
+
+        localStorage.setItem('cityName', JSON.stringify(citySearch));
+
+        function pageLoad() {
+            let lastCity = JSON.parse(localStorage.getItem('cityName'));
+            $('.list-group').append(`<li class="list-group-item"><a href="#">${citySearch}</a></li>`);
+        }
+
         init(citySearch);
+        pageLoad();        
     })
 });
 
@@ -24,7 +35,7 @@ function init(citySearch) {
             var weatherIcon = result.weather[0].icon;
             $('#city-name').text(citySearch + ' (' + currentDay + ') ');
             $('#forecast-image').attr('src', weatherIconURL + weatherIcon + '.png');
-            $('#temperature').html('<b>Temperature: </b>' + result.main.temp + ' °F');
+            $('#temperature').html('<b>Temperature: </b>' + parseInt(result.main.temp) + ' °F');
             $('#humidity').html('<b>Humidity: </b>' + result.main.humidity + '%');
             $('#windspeed').html('<b>Windspeed: </b>' + result.wind.speed + ' MPH');
 
@@ -38,7 +49,7 @@ function init(citySearch) {
                 success: function (uviResult) {
                     var uviResults = uviResult.value
                     $("#uv-index").html('<b>UV Index: </b>' + '<span class="badge" id="uvi-badge">' + uviResults + '</span>');
-                    
+
                     if (uviResults < 3) {
                         $('#uvi-badge').addClass('badge badge-success');
                     } else if (uviResults < 6) {
@@ -46,48 +57,42 @@ function init(citySearch) {
                     } else if (uviResults < 11) {
                         $('#uvi-badge').addClass('badge badge-danger');
                     }
-                
+
                 }
             });
-            
-                }
-                
-            });
 
-            $.ajax({
-                type: 'get',
-                url: forecastAPI + citySearch + units + apiKey,
-                dataType: 'JSON',
-                success: function (response) {
-                    $("#five-day").toggle(".hidden");
-                    var fiveDayFc = response.list;
-                    for (var i =0 ; i< fiveDayFc.length ; i+= 8) {
-                        var fiveDayCards = $("<div class='card-deck card text bg-primary center mx-auto mb-10 p-2' style='color:white; height: 12rem; width: 10rem; margin: 15px;'>");
-                        var date = fiveDayFc[i].dt_txt;
-                        var setDate = date.substr(0,10);
-                        var temp = fiveDayFc[i].main.temp;
-                        var hum = fiveDayFc[i].main.humidity;
-                        var fcIcon = fiveDayFc[i].weather[0].icon
+        }
 
-                        var h6date = $("<h6 class='card-date'>").text(setDate);
-                        var pTemp = $("<p class='weather-temp'>").html('<b>Temp: </b>' + temp + ' °F');
-                        var pHum = $("<p class='weather-humidity'>").html('<b>Humidity: </b>' + hum+ '%');
-                        var fcImg = $('<img>').attr('src', weatherIconURL + fcIcon + '.png');
+    });
 
-                        var weather = fiveDayFc[i].weather[0].main
+    $.ajax({
+        type: 'get',
+        url: forecastAPI + citySearch + units + apiKey,
+        dataType: 'JSON',
+        success: function (response) {
+            $("#five-day").removeClass(".hidden");
+            var fiveDayFc = response.list;
+            for (var i = 0; i < fiveDayFc.length; i += 8) {
+                var fiveDayCards = $("<div class='card-deck card text-center mx-auto mb-8 p-2' style='color:white; background-color: navy; height: 12rem; width: 10rem; margin: 15px;'>");
+                var date = fiveDayFc[i].dt_txt;
+                var setDate = date.substr(0, 10);
+                var temp = fiveDayFc[i].main.temp;
+                var hum = fiveDayFc[i].main.humidity;
+                var fcIcon = fiveDayFc[i].weather[0].icon
 
-                        fiveDayCards.append(h6date);
-                        fiveDayCards.append(pTemp);
-                        fiveDayCards.append(fcImg);
-                        fiveDayCards.append(pHum);
+                var h6date = $("<h6 class='card-date'>").text(setDate);
+                var pTemp = $("<p class='weather-temp'>").html('<b>Temp: </b>' + parseInt(temp) + ' °F');
+                var pHum = $("<p class='weather-humidity'>").html('<b>Humidity: </b>' + parseInt(hum) + '%<br>');
+                var fcImg = $('<img>').attr('src', weatherIconURL + fcIcon + '.png');
 
-                        $("#forecast-container").append(fiveDayCards);
+                var weather = fiveDayFc[i].weather[0].main
 
-                        
-                    }
-                }
-            })
-         }
+                fiveDayCards.append(h6date, pTemp, pHum, fcImg);
 
+                $("#forecast-container").append(fiveDayCards);
 
-        
+            }
+        }
+    })
+}
+
